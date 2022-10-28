f440c2834151167b3d29f0d28c6ff726491f2de2
unknown <omy919@gmail.com>
2022-10-28 05:28:22 -0700

    test static paths

---
+++ components/heading/index.tsx
@@ -0,0 +1,15 @@
+import { IconButton, Typography  } from "@mui/joy";
+import { Tooltip } from "@mui/material";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
+export default function Heading () {
+    return (
+        <Typography level="h3">What&apos;s Trending in US
+          <Tooltip title="Support for other geo(s) coming soon" enterTouchDelay={0} sx={{ marginBottom: '2rem' }}>
+            <IconButton size='sm'>
+              <InfoOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </Typography>
+    );
+} 
