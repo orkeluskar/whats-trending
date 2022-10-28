22f5bef5cb7991ed9a4e76b1775a603cd231e71b
unknown <omy919@gmail.com>
2022-10-28 07:08:24 -0700

    Fix EOF new line

--- components/heading/index.tsx
+++ components/heading/index.tsx
@@ -1,15 +1,15 @@
-import { IconButton, Typography  } from "@mui/joy";
+import { IconButton, Typography } from "@mui/joy";
 import { Tooltip } from "@mui/material";
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
-export default function Heading () {
+export default function Heading() {
     return (
         <Typography level="h3">What&apos;s Trending in US
-          <Tooltip title="Support for other geo(s) coming soon" enterTouchDelay={0} sx={{ marginBottom: '2rem' }}>
-            <IconButton size='sm'>
-              <InfoOutlinedIcon />
-            </IconButton>
-          </Tooltip>
+            <Tooltip title="Support for other geo(s) coming soon" enterTouchDelay={0} sx={{ marginBottom: '2rem' }}>
+                <IconButton size='sm'>
+                    <InfoOutlinedIcon />
+                </IconButton>
+            </Tooltip>
         </Typography>
     );
-} 
+}
