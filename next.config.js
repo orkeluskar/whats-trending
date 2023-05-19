8f088f30153c24d06484e4a6510d3436549fc477
Omkar Keluskar <omy919@gmail.com>
2023-05-19 01:21:35 -0700

    test pwa (#14)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
+
+const runtimeCaching = require('next-pwa/cache')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['dnm.nflximg.net', 'i.scdn.co'],
-  },
+  }
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching,
+})
+
+module.exports = withPWA({
+  ...nextConfig
+})
