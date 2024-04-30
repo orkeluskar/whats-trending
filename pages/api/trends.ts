e2fc38f2d03b835d3a70cc6117b52cc624006406
orkeluskar <orkeluskar@gmail.com>
2024-04-29 20:58:54 -0700

    add logging

--- pages/api/trends.ts
+++ pages/api/trends.ts
@@ -1,11 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { saveTrends } from '../../service/aggregator';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { saveTrends } from "../../service/aggregator";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const response = await saveTrends()
-  res.status(200).json(response);
+  try {
+    const response = await saveTrends();
+    res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    res.status(200).json({ error: e });
+  }
 }
