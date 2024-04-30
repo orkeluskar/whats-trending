9b5ed61e1080a5ff7a6a4956c53d00535a84f0b1
orkeluskar <orkeluskar@gmail.com>
2024-04-29 21:09:27 -0700

    try adding user agent

--- service/reddit.ts
+++ service/reddit.ts
@@ -1,38 +1,44 @@
-import axios from "axios"
-import CONSTANTS from "../utils/constants"
+import axios from "axios";
+import CONSTANTS from "../utils/constants";
 
 export class Reddit {
-    constructor() {
+  constructor() {}
 
-    }
+  private formatResponse(item: any) {
+    const data = item.data;
+    const { subreddit, id, title, thumbnail } = data;
+    const formattedTitle = title.replaceAll(" ", "_");
+    const media_url =
+      !thumbnail?.includes("http") ||
+      thumbnail?.includes("external-preview.redd.it")
+        ? null
+        : thumbnail;
 
-    private formatResponse (item: any) {
-        const data = item.data
-        const { subreddit, id, title, thumbnail } = data
-        const formattedTitle = title.replaceAll(' ', '_')
-        const media_url = !thumbnail?.includes('http') || thumbnail?.includes('external-preview.redd.it') ? null : thumbnail
+    return {
+      name: `/r/${subreddit}/comments/${id}/${formattedTitle}`,
+      volume: data.score?.toLocaleString(),
+      source: CONSTANTS.SOURCE.REDDIT,
+      media_url,
+    };
+  }
 
-        return {
-            name: `/r/${subreddit}/comments/${id}/${formattedTitle}`,
-            volume: data.score?.toLocaleString(),
-            source: CONSTANTS.SOURCE.REDDIT,
-            media_url
-        }
-    }
+  public async getTrendingSearches() {
+    const url = `${process.env.REDDIT_API_URL}/r/popular/hot`;
+    const config = {
+      params: {
+        geo_filter: "US",
+      },
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+      },
+    };
 
-    public async getTrendingSearches() {
-        const url = `${process.env.REDDIT_API_URL}/r/popular/hot`
-        const config = {
-            params: {
-                geo_filter: 'US'
-            }
-        }
-
-        const { data } = await axios.get(url, config)
-        return data?.data?.children?.map(this.formatResponse)
-    }
+    const { data } = await axios.get(url, config);
+    return data?.data?.children?.map(this.formatResponse);
+  }
 }
 
-const redditClient = new Reddit()
+const redditClient = new Reddit();
 
-export default redditClient
+export default redditClient;
